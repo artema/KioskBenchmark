89d415c74c6708c059959b300f5bfea1ec6df116
Artem Abashev <artem@abashev.com>
2013-11-16 12:52:56 +0400

    Added CefGlue Windows Forms project.

---
+++ KioskBenchmark.FormsCefGlue/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefGlue;
+using CefGlue.Windows.Forms;
+using System.Configuration;
+
+namespace KioskBenchmark.FormsCefGlue
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private CefWebBrowser webView;
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            var settings = new CefBrowserSettings();
+            webView = new CefWebBrowser(settings, ConfigurationManager.ConnectionStrings["HomeUrl"].ConnectionString);
+            webView.Dock = DockStyle.Fill;
+            this.Controls.Add(webView);
+        }
+    }
+}
