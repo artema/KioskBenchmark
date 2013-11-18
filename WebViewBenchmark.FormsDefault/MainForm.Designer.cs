506b865a82e92bb1cf6f2b4cc4520e35e1a5afb6
Artem Abashev <artem@abashev.com>
2013-11-18 23:11:23 +0400

    Added default Windows Forms project.

---
+++ WebViewBenchmark.FormsDefault/MainForm.Designer.cs
@@ -0,0 +1,67 @@
+﻿using System.Windows.Forms;
+namespace WebViewBenchmark.FormsDefault
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webView = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+
+            // 
+            // webView
+            // 
+            this.webView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView.Location = new System.Drawing.Point(0, 0);
+            this.webView.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webView.Name = "webView";
+            this.webView.Size = new System.Drawing.Size(1385, 856);
+            this.webView.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1385, 856);
+            this.Controls.Add(this.webView);
+            this.Controls.Add(this.webBrowser1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.WindowState = FormWindowState.Normal;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Bounds = Screen.PrimaryScreen.Bounds;
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webBrowser1;
+        private System.Windows.Forms.WebBrowser webView;
+    }
+}
+
---
+++ WebViewBenchmark.FormsDefault/MainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebViewBenchmark.FormsDefault
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            webView.Url = new Uri(ConfigurationManager.ConnectionStrings["HomeUrl"].ConnectionString);
+        }
+    }
+}
