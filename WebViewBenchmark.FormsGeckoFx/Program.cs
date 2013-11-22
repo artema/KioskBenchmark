21f459e02e4ff138ee29d0e4bdd8307f88dcebfb
Artem Abashev <artem@abashev.com>
2013-11-22 23:23:51 +0400

    Added GeckoFX Windows Forms project.

---
+++ WebViewBenchmark.FormsGeckoFx/Program.cs
@@ -0,0 +1,33 @@
+﻿using Gecko;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebViewBenchmark.FormsGeckoFx
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Xpcom.Initialize(ConfigurationManager.ConnectionStrings["XulRunner"].ConnectionString);
+
+            var form = new Form();
+            form.WindowState = FormWindowState.Normal;
+            form.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            form.Bounds = System.Windows.Forms.Screen.PrimaryScreen.Bounds;
+
+            var browser = new GeckoWebBrowser { Dock = DockStyle.Fill };
+            browser.Navigate(ConfigurationManager.ConnectionStrings["HomeUrl"].ConnectionString);
+            form.Controls.Add(browser);
+            Application.Run(form);
+        }
+    }
+}
